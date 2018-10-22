246aad068bb219503d7012c0387c8f23c09f0eb4
Alice Francener <alicefrancener@gmail.com>
2018-10-22 11:29:27 -0300

    Inferencia de variaveis com erro

--- CursoKotlin/src/fundamentos/CuidadoInferencia1.kt
+++ CursoKotlin/src/fundamentos/CuidadoInferencia1.kt
@@ -1,2 +1,11 @@
 package fundamentos
 
+fun main(args: Array<String>) {
+    //var a
+    // erro, porque não inferiu tipo e também não inicializou
+
+    var a = 10
+    var b = 2
+
+    print(a + b)
+}
