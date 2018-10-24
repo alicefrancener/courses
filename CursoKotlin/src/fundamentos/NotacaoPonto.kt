0eb5345603b6d24e2742a4138a4179edb0aefbc5
Alice Francener <alicefrancener@gmail.com>
2018-10-24 16:47:51 -0300

    Notação ponto

---
+++ CursoKotlin/src/fundamentos/NotacaoPonto.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val a: Int = 33.dec() //decrementar
+    var b: String = a.toString()
+
+    println("Int: $a")
+    println("Primeiro char da string b é: ${b.first()}")
+    println("Minha string é $b")
+}
