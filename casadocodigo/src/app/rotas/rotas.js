6ff40f415e36890aa8e84f65d384df4307048a35
Alice Francener <alicefrancener@gmail.com>
2019-08-05 16:30:27 -0300

    Acessa conteúdo armazenado em .marko

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -15,15 +15,8 @@ module.exports = (app) => {
   });
 
   app.get('/livros', function (req, resp) {
-    resp.send(`
-  <html>
-      <head>
-         <meta charset="utf-8">
-      </head>
-      <body>
-          <h1> Listagem de Livros </h1>
-      </body>
-  </html>
-  `);
+    resp.marko(
+      require('../views/livros/lista/lista.marko')
+    );
   });
 }
