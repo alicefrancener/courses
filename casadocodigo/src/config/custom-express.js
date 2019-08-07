21d6fb748ff2b360ed8c5b4513374b47068d2b8b
alicefrancener <alicefrancener@gmail.com>
2019-08-07 16:24:23 -0400

    Inclui middleware

--- casadocodigo/src/config/custom-express.js
+++ casadocodigo/src/config/custom-express.js
@@ -3,6 +3,11 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser'); //middleware
+
+app.use(bodyParser.urlencoded({
+  extended: true //habilitado a receber objetos complexos em formato JSON
+}));
 
 const rotas = require('../app/rotas/rotas');
 rotas(app);
