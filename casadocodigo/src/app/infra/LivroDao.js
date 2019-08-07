33822b94699818e7101b7fdf66e757ef00b32319
alicefrancener <alicefrancener@gmail.com>
2019-08-07 19:26:23 -0400

    Implementa método para adicionar livro

--- casadocodigo/src/app/infra/LivroDao.js
+++ casadocodigo/src/app/infra/LivroDao.js
@@ -4,6 +4,29 @@ class LivroDao {
     this._db = db;
   }
 
+  adiciona(livro) {
+    return new Promise((resolve, reject) => {
+      
+      // db.run instrucao do sqlite que nao retorna resultado - como insercao, delecao e atualizacao
+      /* parametros: instrucao; 
+       * array com as informacoes que substituirao as interrogacoes ; 
+       * funcao callback que sera executa ao final
+       */
+      this._db.run(`INSERT INTO livros (titulo, preco, descricao) values (?,?,?)`, [
+          livro.titulo,
+          livro.preco,
+          livro.descricao
+        ],
+        function(err) {
+          if (err) {
+            console.log(err);
+            return reject('Não foi possível adiciona o livro!');
+          }
+          resolve();
+        });
+    })
+  }
+
   lista() {
     return new Promise((resolve, reject) => {
       this._db.all(
