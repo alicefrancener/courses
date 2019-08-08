f7fc2b58a5f015d524a61d03916b725f2f334927
alicefrancener <alicefrancener@gmail.com>
2019-08-08 16:46:44 -0400

    Implementa função para encontrar livro pelo ID

--- casadocodigo/src/app/infra/LivroDao.js
+++ casadocodigo/src/app/infra/LivroDao.js
@@ -6,13 +6,13 @@ class LivroDao {
 
   adiciona(livro) {
     return new Promise((resolve, reject) => {
-      
+
       // db.run instrucao do sqlite que nao retorna resultado - como insercao, delecao e atualizacao
       /* parametros: instrucao; 
        * array com as informacoes que substituirao as interrogacoes ; 
        * funcao callback que sera executa ao final
        */
-      this._db.run(`INSERT INTO livros (titulo, preco, descricao) values (?,?,?)`, [
+      this._db.run('INSERT INTO livros (titulo, preco, descricao) values (?,?,?)', [
           livro.titulo,
           livro.preco,
           livro.descricao
@@ -20,7 +20,7 @@ class LivroDao {
         function(err) {
           if (err) {
             console.log(err);
-            return reject('Não foi possível adiciona o livro!');
+            return reject('Não foi possível adicionar o livro!');
           }
           resolve();
         });
@@ -39,6 +39,23 @@ class LivroDao {
       )
     });
   }
+
+  buscaPorId(id) {
+    return new Promise((resolve, reject) => {
+      this._db.get(
+        `SELECT * 
+         FROM livros 
+         WHERE id = ?`, [id],
+        (err, livro) => {
+          if (err) {
+            return reject('Não foi possível encontrar o livro pela ID!');
+          }
+          resolve();
+        }
+      );
+    });
+  }
+
 }
 
 module.exports = LivroDao;
