8bb52302370862bb06256fa0be23ed63272af0e3
Alice Francener <alicefrancener@gmail.com>
2019-08-10 21:51:24 -0400

    Implementa código de remoção de item e middleware

--- casadocodigo/src/config/custom-express.js
+++ casadocodigo/src/config/custom-express.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); //middleware
 
+//middleware para acessar arquivos estaticos
+app.use('/estatico', express.static('src/app/public'));
+
 app.use(bodyParser.urlencoded({
   extended: true //habilitado a receber objetos complexos em formato JSON
 }));
