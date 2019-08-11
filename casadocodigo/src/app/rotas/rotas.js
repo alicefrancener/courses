1e4b76e590241aef5df1ad43d8c8ab3d0fc02494
Alice Francener <alicefrancener@gmail.com>
2019-08-10 22:00:21 -0400

    Cria rota para edição de livro

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -30,7 +30,23 @@ module.exports = (app) => {
   });
 
   app.get('/livros/form', function(req, resp) {
-    resp.marko(require('../views/livros/form/form.marko'));
+    resp.marko(require('../views/livros/form/form.marko'), {
+      livro: {}
+    });
+  });
+
+  app.get('/livros/form', function(req, resp) {
+    const id = req.params.id;
+    const livroDao = new LivroDao(db);
+    livroDao.buscaPorId(id)
+      .then(livro =>
+        resp.marko(
+          require('../views/livros/form/form.marko'), {
+            livro: livro
+          }
+        )
+      )
+      .catch(erro => console.log(erro));
   });
 
   app.post('/livros', function(req, resp) {
@@ -40,13 +56,13 @@ module.exports = (app) => {
       .then(resp.redirect('/livros')) //quando terminar de add o livro ao bd, redireciona para listagem de livros
       .catch(erro => console.log(erro));
   });
-  
+
   app.delete('/livros/:id', function(req, resp) {
     const id = req.params.id;
     const livroDao = new LivroDao(db);
     livroDao.remove(id)
-    .then(() => resp.status(200).end())
-    .catch(erro => console.log(erro));
+      .then(() => resp.status(200).end())
+      .catch(erro => console.log(erro));
   });
 
 }
