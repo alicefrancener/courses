866ecde6dc2e01f878cabda3189a7f38194f56d5
Alice Francener <alicefrancener@gmail.com>
2019-10-23 16:41:19 -0300

    calcula IMC para todos pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,28 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Nutricionista';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+    var peso = pacientes[i].querySelector('.info-peso').textContent;
+    var altura = pacientes[i].querySelector('.info-altura').textContent;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        pacientes[i].querySelector('.info-peso').textContent = 'Peso Inválido';
+    }
+
+    if (alta <= 0 || altura >= 3.0) {
+        alturaEhValida = false;
+        pacientes[i].querySelector('.info-altura').textContent = 'Peso Inválido';
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        pacientes[i].querySelector('.info-imc').textContent = 'Dados inválidos';
+    }
+
+    var imc = peso / (altura * altura);
+    pacientes[i].querySelector('.info-imc').textContent = imc.toFixed(2);
+}
