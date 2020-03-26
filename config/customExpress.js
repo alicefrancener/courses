0bcd9e824a4209a09381538ab5ff94276ba62256
Alice Francener <alicefrancener@gmail.com>
2020-03-26 14:02:11 -0300

    chore: add body-parser

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const consign = require("consign");
 
 module.exports = () => {
   const app = express();
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
   consign()
     .include("controllers")
