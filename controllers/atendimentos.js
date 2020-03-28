81b5b2557d169c978559d5b513fd6329f2ec6a71
Alice Francener <alicefrancener@gmail.com>
2020-03-28 08:53:48 -0300

    feat: add GET

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,17 @@
 const Atendimento = require("../models/atendimentos");
 
 module.exports = app => {
-  app.get("/atendimentos", (req, res) => res.send("Hello World!"));
+  app.get("/atendimentos", (req, res) => {
+    Atendimento.lista(res);
+  });
 
-  app.post('/atendimentos', (req, res) => {
-    const atendimento = req.body
-    Atendimento.adiciona(atendimento, res)
+  app.get("/atendimentos/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    Atendimento.buscaPorId(id, res);
   })
+
+  app.post("/atendimentos", (req, res) => {
+    const atendimento = req.body;
+    Atendimento.adiciona(atendimento, res);
+  });
 };
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,9 +1,27 @@
 const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
+  lista(res) {
+    const sql = "SELECT * FROM Atendimentos";
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) res.status(400);
+      else res.status(200).json(resultados);
+    });
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) res.status(400);
+      else {
+        const atendimento = resultados[0];
+        res.status(200).json(atendimento);
+      }
+    });
+  }
+
   adiciona(atendimento, res) {
     const sql = "INSERT INTO Atendimentos SET ?";
-
     conexao.query(sql, atendimento, (erro, resultados) => {
       if (erro) res.status(400);
       else res.status(201).json(resultados);
