1b1c3e8bf77e8442b586fda075c8b95822b78e6a
Alice Francener <alicefrancener@gmail.com>
2020-03-28 19:48:19 -0300

    refactor: change express to graphql

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const customExpress = require('./config/custom-express')
-const conexao = require('./infraestrutura/conexao')
-const Tabelas = require('./infraestrutura/database/tabelas')
-
-const app = customExpress()
+const { GraphQLServer } = require("graphql-yoga");
+const conexao = require("./infraestrutura/conexao");
+const Tabelas = require("./infraestrutura/database/tabelas");
 
 conexao.connect(erro => {
   if (erro) {
-    console.log(erro)
+    console.log(erro);
   }
+  console.log("conectou no banco");
+  Tabelas.init(conexao);
+});
 
-  console.log('conectou no banco')
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando"
+  }
+};
 
-  Tabelas.init(conexao)
-})
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: "./schema.graphql"
+});
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
-})
+servidor.start(() => console.log("Servidor ouvindo"));
